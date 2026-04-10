3fc866fb0015d99f168c9cb67a8ccca794d32ecf
Vedant Arya <vedant.arya0599@gmail.com>
2026-04-10 15:14:30 +0530

    List tut. completed

---
+++ Bro_Code/Day_2/list.py
@@ -0,0 +1,16 @@
+#This is first tutorial of list 
+list1 = ["cat", "dog", "Dog", "cat", "parrot","pandas"]
+# print(list1)
+# print(list1[2])
+# for p in list1:
+#     print(p, end=" ")
+# list1.sort() #here is twist when i use this sort() it's sorting capital letter and small letter word also 
+# print(list1)
+# list1.append("cow")
+# print(list1)
+# list1.pop() #there is no change after using this function with list
+# print(help(list1))
+# list1.reverse()
+list1.remove("parrot") 
+print(list1)
+#There are many function for list 
---
+++ Bro_Code/Day_2/tempCodeRunnerFile.py
@@ -0,0 +1,4 @@
+() #here is twist when i use this sort() it's sorting capital letter and small letter word also 
+# print(list1)
+# list1.append("cow")
+# print(list1)
