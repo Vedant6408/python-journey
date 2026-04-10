da74902f1fc1b01bf8533a22b7cd25ae1e9715c5
Vedant Arya <vedant.arya0599@gmail.com>
2026-04-10 12:19:43 +0530

    Time module first programc completed

--- Bro_Code/Day_2/time_module.py
+++ Bro_Code/Day_2/time_module.py
@@ -6,7 +6,10 @@ import time
 usr_time = int(input("Enter the time in second: "))
 # for i in reversed(range(0,usr_time)):
 for i in range(usr_time,0,-1):
+    second = i % 60
+    minutes = int(i / 60) % 60
+    hours = int(i / 3600)
     # time.sleep(usr_time)
-    time.sleep(usr_time) #there is no difference after swaping line 9 to 10 
-    print(i)
+    time.sleep(1) #there is no difference after swaping line 9 to 10 
+    print(f"{hours:02}:{minutes:02}:{second:02}")
 print("Time's UP!")
