c7b6d59661419d4c6f160ceda5673c61304a5afc
Vedant Arya <vedant.arya0599@gmail.com>
2026-04-10 12:01:21 +0530

    range function usage

--- Bro_Code/Day_2/time_module.py
+++ Bro_Code/Day_2/time_module.py
@@ -1,4 +1,12 @@
 import time
 
-time.sleep(4)
-print("Times Up !")
+# time.sleep(4)
+# print("Times Up !")
+
+usr_time = int(input("Enter the time in second: "))
+# for i in reversed(range(0,usr_time)):
+for i in range(usr_time,0,-1):
+    # time.sleep(usr_time)
+    time.sleep(usr_time) #there is no difference after swaping line 9 to 10 
+    print(i)
+print("Time's UP!")
