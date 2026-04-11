43efb8bf11c4d23238da271098444d0d3e6c1852
Vedant Arya <vedant.arya0599@gmail.com>
2026-04-12 00:30:13 +0530

    srps

--- Bro_Code/Basic_tutorials/Reels_problem_solution.py
+++ Bro_Code/Basic_tutorials/Reels_problem_solution.py
@@ -26,9 +26,32 @@
 # something = usr_input2 % 60
 # print(something)
 
-cars = ["Porsche", "BMW", "Audi"]
-trending = []
-for c in cars:
-    if c == "BMW" or "Porsche":
-        trending.append(c)
-print(trending)
+# cars = ["Porsche", "BMW", "Audi"]
+# trending = []
+# for c in cars:
+#     if c == "BMW" or "Porsche":
+#         trending.append(c)
+# print(trending)
+
+# fruits = ["apple", "banana", "mango", "kiwi", "grape"]
+# for f in fruits:
+#     if "a" in f:
+#         fruits.remove(f)
+# print(fruits)
+
+a = [11,22,33,44,55,66]
+for i in a:
+    a.remove(i)
+print(i)
+
+x = True
+if x == 1:
+    print("stephen hawking")  #here is the twist once the if statement if true i will not read the elif state after thatif x is True:     
+     #but if we use if statement after if statement then what will gonna happen let's see
+if x is True:
+    print('python') #it also print that python string
+elif x:
+    print("the breif history of time")
+else:
+    print("cosmos")
+#if the if statement is true then all the elif is going to be ignored but if there will be 2 or more if statement then they will also gonna be read by the compiler
