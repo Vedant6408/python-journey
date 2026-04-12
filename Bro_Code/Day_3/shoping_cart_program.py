ed1722d52d05291bcf78362366bcd693cdec7121
Vedant Arya <vedant.arya0599@gmail.com>
2026-04-12 18:58:38 +0530

    .

---
+++ Bro_Code/Day_3/shoping_cart_program.py
@@ -0,0 +1,8 @@
+print("="*30)
+wel = "WELCOME"
+print(f"{wel.center(30,"-")}")
+print("="*30)
+stock_list_inp = input("do you wanna se what we've in stock?(y/n)")
+fruit_list = []
+fruit_price = []
+if stock_list_inp.capitalize == 
