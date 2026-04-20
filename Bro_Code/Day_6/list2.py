b6c01ff8079400618be7e2c93da0f6bdf98efe94
Vedant Arya <vedant.arya0599@gmail.com>
2026-04-20 15:32:17 +0530

    .

--- Bro_Code/Day_6/list2.py
+++ Bro_Code/Day_6/list2.py
@@ -1,5 +1,15 @@
-lst1 = ["Apple","Oragne","Grapes","Strawberry"]
-lst2 = [2 , 3, 8, 9, 10]
-lst3 = [3.125, 7.05, 8,"Cat", "Dog"]
+# fruits = ["Apple","Orange","Strawberry","Blue Berries"]
+# vegetables = ["Potato","Tomato","Ginger","Lady's Fingers","Groud","Bitter Groud"]
+# Non_veg_items = ["Egg","Meat","Chicken","Fish"]
+# matrix = [fruits,vegetables,Non_veg_items]
+# print(matrix[2][2])
 
-print()
+#We can also define 2dmatrix without defining 3 or move variable 
+
+grocries = [
+    ["Close up", "Colgate","Colgate maxfresh"],
+    ["Mixture","Biscuit","Kurkure","Chips"],
+    ["Wheel","Surf excel"]
+]
+
+print(grocries[2][1]) #third rows are the rows of detergent poweder brand and i acces that surf excel element
