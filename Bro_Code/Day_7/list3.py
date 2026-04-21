4756ddeda47023fd16f75199bdc7629679ccf65d
Vedant Arya <vedant.arya0599@gmail.com>
2026-04-21 20:18:32 +0530

    -quiz game

--- Bro_Code/Day_7/list3.py
+++ Bro_Code/Day_7/list3.py
@@ -1,9 +1,20 @@
-groceries = [ 
-    ["Sprite","Thums-Up","Coca-cola","Pepsi","Mazza"],
-    ["Realme","Samsung","Xioami","Apple","Asus"]
-]
-# print(groceries)
-for a in groceries:
-    for j in a:
-        print(f"___{j}")
+# groceries = [ 
+#     ["Sprite","Thums-Up","Coca-cola","Pepsi","Mazza"],
+#     ["Realme","Samsung","Xioami","Apple","Asus"]
+# ]
+# # print(groceries)
+# for a in groceries:
+#     for j in a:
+#         print(j, end=" ")
+#     print()
 
+num_pad = (
+    (1,2,3),
+    (4,5,6),
+    (7,8,9),
+    ("*",0,"#")
+)
+for numbers in num_pad:
+    for keys in numbers:
+        print(keys, end=" ")
+    print()
---
+++ Bro_Code/Day_7/quiz_game.py
@@ -0,0 +1,6 @@
+question = (
+    ["Which metal has the highest melting point?:  "],
+    ("How many colour can human eye see ?: "),
+    ["What is i"]
+)
+option = ((),(),(),(),())
