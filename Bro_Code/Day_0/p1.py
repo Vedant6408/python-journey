9599d2be214836934ebf10dda7fd1098a3ca6c9c
Vedant Arya <vedant.arya0599@gmail.com>
2026-05-05 17:31:10 +0530

    day0

---
+++ Bro_Code/Day_0/p1.py
@@ -0,0 +1,8 @@
+#1
+# a = input("Enter your first name: ")
+# b = input("Enter your last name: ")
+# print(f"This is your full name: {a.capitalize()} {b.capitalize()}")
+
+#2
+c = input("Enter symbol: ")
+print(c*10)
