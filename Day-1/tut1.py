bc9018f97dc2bd5c5476a4c2cdeec4f398c6632d
Vedant Arya <vedant.arya0599@gmail.com>
2026-06-03 15:41:44 +0530

    few practices

---
+++ Day-1/tut1.py
@@ -0,0 +1,15 @@
+# #1 
+# var1 = 2
+# var2 = 2.05
+# var3 = "storing string"
+# var4 = True
+# var5 = False
+# print(var1, var2, var3, var4, var5)
+# print(f"printing all variable value{var1,var2,var3,var4,var5}")
+
+#2
+a = 25
+b = 5
+c,d = 20,4
+print(f"Ans:{int(c/d)}")
+print(f"Ans:",a/b,"\n",a*b)
